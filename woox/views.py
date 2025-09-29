435aeb48a230e5d237bd5be725efaee43a6b7f46
pinakinilucky <pinakiniinusa@gmail.com>
2025-09-29 15:00:55 -0400

    new message3

--- woox/views.py
+++ woox/views.py
@@ -4,3 +4,5 @@ from .models import Destination
 def index(request):
    destinations=Destination.objects.all().order_by('display_order')
    return render(request,'index.html', {'destinations': destinations})
+def switzerland(request):
+   return render(request,'switzerland.html')
